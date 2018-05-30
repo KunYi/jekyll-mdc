96555544b8ae64995df58abfacd1ce6569b5a83f
Chen, Kun-Yi(陳坤毅) <kunyi@uwingstech.com>
2018-05-30 23:23:34 +0800

    add scrolling hacking for select item

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,13 @@
 //=require bundle
+
+// add scroll section hack functions
+function jsel(label) {
+    $('html, body').animate({
+        scrollTop: $("#"+label).offset().top -
+        $('.mdc-top-app-bar').height() - 2
+    }, 1000);
+
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+    });
+}
