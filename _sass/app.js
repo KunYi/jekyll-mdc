4b510e41a04408f3258ac2ba41c71b97f6eae2ef
Chen, Kun-Yi(陳坤毅) <kunyi@uwingstech.com>
2018-05-30 13:57:48 +0800

    add menu item

--- _sass/app.js
+++ _sass/app.js
@@ -2,5 +2,25 @@ import {MDCTopAppBar} from '@material/top-app-bar/index';
 import {MDCMenu} from '@material/menu';
 
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
-const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+
+// for menu
+var menuEl = document.querySelector('#menu');
+var menu = new MDCMenu(menuEl);
+var menuButtonEl = document.querySelector('#menu-button');
+
+// Toggle menu open
+menuButtonEl.addEventListener('click', function() {
+  menu.open = !menu.open;
+});
+
+// Listen for selected item
+menuEl.addEventListener('MDCMenu:selected', function(evt) {
+   var detail = evt.detail;
+});
+
+// Set Anchor Corner to Bottom End
+menu.setAnchorCorner(Corner.BOTTOM_END);
+
+// Turn off menu open animations
+menu.quickOpen = true;
 
