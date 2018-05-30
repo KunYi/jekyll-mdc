23192402db5aabbc29d38e15ea992defb1e2c878
KunYi Chen <kunyi@uwingstech.com>
2018-05-30 15:25:42 +0000

    Deploy KunYi/jekyll-mdc to github.com/KunYi/jekyll-mdc.git:gh-pages

--- assets/main-d634cfe5783a88932d93f54f2efc020acee9dd843ae25acecf829a1f87441084.js
+++ assets/main-d634cfe5783a88932d93f54f2efc020acee9dd843ae25acecf829a1f87441084.js
@@ -4623,3 +4623,17 @@ exports.MDCMenuAdapter = MDCMenuAdapter;
 
 /***/ })
 /******/ ]);
+
+
+// add scroll section hack functions
+function jsel(label) {
+    $('html, body').animate({
+        scrollTop: $("#"+label).offset().top -
+        $('.mdc-top-app-bar').height() - 2
+    }, 1000);
+
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+    });
+}
+;
