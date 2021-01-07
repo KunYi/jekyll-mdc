e5c68e4529b44e0c0a86b4c3a98f602b27452f40
KunYi <kunyi.chen@gmail.com>
2021-01-07 11:02:05 +0800

    Revert "update npm and package"
    This reverts commit 42b75d43f072642f5bb32101e3fca4e298c6c6b9.

--- webpack.config.js
+++ webpack.config.js
@@ -14,27 +14,33 @@ module.exports = [{
     rules: [{
       test: /\.scss$/,
       use: [
-        { loader: 'file-loader',
+        {
+          loader: 'file-loader',
           options: {
-			      path: path.resolve(__dirname, "_sass/vendor"),
-            name: '_bundle.scss'
-          }
+			path: path.resolve(__dirname, "_sass/vendor"),
+            name: '_bundle.scss',
+          },
         },
         { loader: 'extract-loader' },
         { loader: 'css-loader' },
-        { loader: 'sass-loader' },
-		    { loader: 'postcss-loader',
-		      options: {
-			      plugins: function () {
-				      return [
-                autoprefixer({ browsers: ['last 4 versions', '> 1%', 'IE >= 9']})
-              ]
-			      }
-		      }
-		    }
+        { loader: 'sass-loader',
+		  options: {
+		    includePaths: ['./node_modules']
+          }
+		},
+		{ loader: 'postcss-loader',
+		  options: {
+			  plugins: function () {
+				return [
+					autoprefixer({
+						browsers: ['last 4 versions', '> 1%', 'IE >= 9']
+				})]
+			  }
+		  }
+		}
       ]
     }]
-  }
+  },
 }];
 
 // webpack folder’s entry js — excluded from jekll’s build process.
@@ -48,14 +54,12 @@ module.exports.push({
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['@babel/preset-env', { targets: "defaults" }]
-          ]
-        }
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
     }]
-  }
+  },
 });
